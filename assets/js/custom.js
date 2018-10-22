6ac270428ea8d8001920b7449ac1c902ee3c6ec3
saddam <saddam@zeepzoop.com>
2018-10-22 13:52:08 +0530

    custome js chnags

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,47 +2,18 @@ $(function() {
     var startStores = 12;
     var endStores = startStores + 11;
 
-    function GetParameterValues(param) {
-        var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for (var i = 0; i < url.length; i++) {
-            var urlparam = url[i].split('=');
-            if (urlparam[0] == param) {
-                return urlparam[1];
-            }
-        }
-    }
+    function GetParameterValues(param) { var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); for (var i = 0; i < url.length; i++) { var urlparam = url[i].split('='); if (urlparam[0] == param) { return urlparam[1]; } } }
     var numberReg = /^[0-9]+$/;
     var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    $('.profilesubmit').click(function() {
-        if ($('#nameInput').val() == "") {
-            console.log("in the if")
-        } else {
-            console.log("in else")
-        }
-    });
-
-    $('.catlg').click(function() {
-        var cityName = GetParameterValues('city');
-        if (cityName) {
-            window.location.href = $(this).attr('name') + "&city=" + cityName;
-        } else {
-            window.location.href = $(this).attr('name');
-        }
-    });
-
+    $('.profilesubmit').click(function() { if ($('#nameInput').val() == "") { console.log("in the if") } else { console.log("in else") } });
+    $('.catlg').click(function() { var cityName = GetParameterValues('city'); if (cityName) { window.location.href = $(this).attr('name') + "&city=" + cityName; } else { window.location.href = $(this).attr('name'); } });
     $('.newsebtn').click(function() {
         var cityName = GetParameterValues('city');
         localStorage.setItem('bread', $(this).attr('id'))
-        if (cityName) {
-            window.location.href = $(this).attr('name') + "&city=" + cityName;
-        } else {
-            window.location.href = $(this).attr('name');
-        }
+        if (cityName) { window.location.href = $(this).attr('name') + "&city=" + cityName; } else { window.location.href = $(this).attr('name'); }
     });
-
     $('#loadmoreshop').click(function() {
         var catid = '';
-
         var urls = "https://api.zeepzoop.com/stores/search?startStores=" + startStores + "&endStores=" + endStores;
         if ($(this).attr('name') == 'Home-Decor') {
             catid = "5a51e626ff645771bf57dc1a"
@@ -57,28 +28,12 @@ $(function() {
             catid = "5a51e544ff645771bf57dc16"
             urls += "&category=" + catid;
         }
-
         var buisnessOffline = GetParameterValues('buisnessOffline');
         var buisnessOnline = GetParameterValues('buisnessOnline');
         var cityName = GetParameterValues('city');
-
-        if (buisnessOffline) {
-            urls += '&buisnessOffline=true';
-        } else if (buisnessOnline) {
-            urls += '&buisnessOnline=true';
-        }
-        if (cityName) {
-            urls += '&location=' + cityName;
-        }
-        var request = $.ajax({
-            url: urls,
-            type: "get",
-            dataType: 'JSON',
-            headers: {
-                'Authorization': "maximumvsminimumsecurity",
-                'Content-Type': "application/json"
-            },
-        });
+        if (buisnessOffline) { urls += '&buisnessOffline=true'; } else if (buisnessOnline) { urls += '&buisnessOnline=true'; }
+        if (cityName) { urls += '&location=' + cityName; }
+        var request = $.ajax({ url: urls, type: "get", dataType: 'JSON', headers: { 'Authorization': "maximumvsminimumsecurity", 'Content-Type': "application/json" }, });
         request.done(function(response) {
             startStores = startStores + 12;
             endStores = startStores + 11;
@@ -87,52 +42,23 @@ $(function() {
                 $('#loadmoreshop').remove();
             }
             html = '';
-            for (i = 0; i < response.data.length; i++) {
-                if (response.data[i].storeCity !== 'Surat' && response.data[i].storeCity !== 'Vadodara')
-                    html += '<div class="bagsmore-item">' +
-                    '<img src="https://api.zeepzoop.com/' + response.data[i].storeBanner + '" alt="">' +
-                    '<div class="watermark-logo"> <img src="/images/img-watermark-icon.png" alt=""> <span>' + response.data[i].avgRating + '</span> </div>' +
-                    '<div class="info-share-book">' +
-                    '<div class="bag-info-cont"> <a href="/brand/' + response.data[i].URL + '" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden;" class="bag-title">' + response.data[i].storeName + '</a>' +
-                    '<p style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">' + response.data[i].storeDiscription + '</p></div>' +
-                    '</div></div>';
-            }
-            $('.mid-scn-main .bagsmore-list-main').append(html);
+            for (i = 0; i < response.data.length; i++) { if (response.data[i].storeCity !== 'Surat' && response.data[i].storeCity !== 'Vadodara') html += '<div class="bagsmore-item">' + '<img src="https://api.zeepzoop.com/' + response.data[i].storeBanner + '" alt="">' + '<div class="watermark-logo"> <img src="images/img-watermark-icon.png" alt=""> <span>' + response.data[i].avgRating + '</span> </div>' + '<div class="info-share-book">' + '<div class="bag-info-cont"> <a href="/brand/' + response.data[i].URL + '" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden;" class="bag-title">' + response.data[i].storeName + '</a>' + '<p style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">' + response.data[i].storeDiscription + '</p></div>' + '</div></div>'; } $('.mid-scn-main .bagsmore-list-main').append(html);
         });
-
         request.fail(function(jqXHR, textStatus) {
             console.log("Request failed: " + textStatus);
             alert("Request failed: " + textStatus);
         });
     });
-
     var startcatalouge = 21;
     var endcatalouge = startcatalouge + 22;
-
     $('#loadcatalouge').click(function() {
         var urls = "https://api.zeepzoop.com/catalogs/featurecatalog?startCatalogs=" + startcatalouge + "&endCatalogs=" + endcatalouge;
         var buisnessOffline = GetParameterValues('buisnessOffline');
         var buisnessOnline = GetParameterValues('buisnessOnline');
         var cityName = GetParameterValues('city');
-
-        if (buisnessOffline) {
-            urls += '&buisnessOffline=true';
-        } else if (buisnessOnline) {
-            urls += '&buisnessOnline=true';
-        }
-        if (cityName) {
-            urls += '&location=' + cityName;
-        }
-        var request = $.ajax({
-            url: urls,
-            type: "get",
-            dataType: 'JSON',
-            headers: {
-                'Authorization': "maximumvsminimumsecurity",
-                'Content-Type': "application/json"
-            },
-        });
-
+        if (buisnessOffline) { urls += '&buisnessOffline=true'; } else if (buisnessOnline) { urls += '&buisnessOnline=true'; }
+        if (cityName) { urls += '&location=' + cityName; }
+        var request = $.ajax({ url: urls, type: "get", dataType: 'JSON', headers: { 'Authorization': "maximumvsminimumsecurity", 'Content-Type': "application/json" }, });
         request.done(function(response) {
             startcatalouge = startcatalouge + 21;
             endcatalouge = startcatalouge + 22;
@@ -141,25 +67,13 @@ $(function() {
                 $('#loadcatalouge').remove();
             }
             html = '';
-            for (i = 0; i < response.data.length; i++) {
-
-                if (response.data[i] && response.data[i].storeURL !== null) {
-                    html += '<div class="catalouges-item">' +
-                        '<div class="catalouges-pic">' +
-                        '<a href="/brand/' + response.data[i].storeURL + '""><img class="catimagesize" src="https://api.zeepzoop.com/' + response.data[i].catalogUrl + '"alt="" /></a>' +
-                        '</div>' +
-                        '</div>';
-                }
-            }
-            $('.mid-scn-main .catalouges-pdt-main').append(html);
+            for (i = 0; i < response.data.length; i++) { if (response.data[i] && response.data[i].storeURL !== null) { html += '<div class="catalouges-item">' + '<div class="catalouges-pic">' + '<a href="/brand/' + response.data[i].storeURL + '""><img class="catimagesize" src="https://api.zeepzoop.com/' + response.data[i].catalogUrl + '"alt="" /></a>' + '</div>' + '</div>'; } } $('.mid-scn-main .catalouges-pdt-main').append(html);
         });
-
         request.fail(function(jqXHR, textStatus) {
             console.log("Request failed: " + textStatus);
             alert("Request failed: " + textStatus);
         });
     });
-
     $('select[name="cities"]').change(function() {
         var $this = $(this);
         var cityname = $this.val();
@@ -167,9 +81,7 @@ $(function() {
         window.location.href = "/?city=" + cityname + " ";
     });
 });
-
 $(document).ready(function() {
-
     if (localStorage.getItem('storetype') && localStorage.getItem('storetype') == "online") {
         $('select[name="cities"]').css("display", "none");
         $('.onlinecollectiondiv').css("display", "-webkit-inline-box");
@@ -191,7 +103,6 @@ $(document).ready(function() {
         $('.onlineoffer').css("display", "none");
         $('.offlineoffer').css("display", "block");
     }
-
     $('.shops-slt').click(function(e) {
         $(this).toggleClass('shopsSlt-current')
         if ($(this).hasClass('shopsSlt-current')) {
@@ -218,21 +129,12 @@ $(document).ready(function() {
             $('.onlineoffer').css("display", "block");
         }
     });
-
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 25) {
-            $('body').addClass('body-fixed');
-        } else {
-            $('body').removeClass('body-fixed');
-        }
-    })
+    $(window).on('scroll', function() { if ($(window).scrollTop() > 25) { $('body').addClass('body-fixed'); } else { $('body').removeClass('body-fixed'); } })
     $(window).trigger('scroll')
-
     $('.nav-toggle').click(function(e) {
         $('.sidebarmenu').addClass('sidebarmenu-show')
         $('.overlay-bg').addClass('overlay-bg-show')
     });
-
     $('.side-menu-close, .overlay-bg').click(function(e) {
         $('.sidebarmenu').removeClass('sidebarmenu-show')
         $('.overlay-bg').removeClass('overlay-bg-show')
